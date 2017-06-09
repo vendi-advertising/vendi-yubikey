4736933991ee7bf4cf98f6f54b4db1b618395df9
Chris Haas <chris@vendiadvertising.com>
2017-06-09 08:35:16 -0500

    Initial test stuff

---
+++ tests/bootstrap.php
@@ -0,0 +1,12 @@
+<?php
+
+$_tests_dir = getenv( 'WP_TESTS_DIR' );
+if ( ! $_tests_dir ) {
+    $_tests_dir = '/tmp/wordpress-tests-lib';
+}
+
+// // Give access to tests_add_filter() function.
+require_once $_tests_dir . '/includes/functions.php';
+
+// Start up the WP testing environment.
+require_once $_tests_dir . '/includes/bootstrap.php';
